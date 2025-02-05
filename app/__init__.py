fb3e428815fce7eceea6acac35ffbc7b1110290c
Conor Kellett <conorplayz11@gmail.com>
2025-02-05 15:56:52 +1100

    Created the database models.

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "you-will-never-guess"
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-from app import routes
+from app import routes, models
